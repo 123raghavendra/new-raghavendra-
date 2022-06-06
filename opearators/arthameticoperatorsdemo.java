7e323e94bbcaa476505bdd08c97d9bc47efd6c14
unknown <raghavendra123xworkz@gmail.com>
2022-06-06 18:17:10 +0530

    operators example

---
+++ opearators/arthameticoperatorsdemo.java
@@ -0,0 +1,36 @@
+class arthameticoperatorsdemo{
+public static void main(String[] args){
+// declone variable
+int a=12,b=5;
+// aditation operator
+System.out.println("sum="+(a+b));
+// substraction operator
+System.out.println("sub="+(a-b));
+// multiplication operator
+System.out.println("mul="+(a*b));
+// division operator
+System.out.println("div="+(a/b));
+// modulo operator
+System.out.println("mod="+(a%b));
+
+
+	int num1=10,num2 =20;
+	int sum;
+	// displaying num1and num2
+	//System.out.println("num1="+num1);
+	// adding num1 and num2
+	sum =num1+num2;
+	System.out.println("the sum =" +sum);
+	
+	
+	int x=420;
+	int y=240;
+	System.out.println(x++);
+	System.out.println(y++);
+        System.out.println(++x);
+        System.out.println(++y);
+        System.out.println(x++);
+        System.out.println(x);
+        System.out.println(y);
+}
+}
