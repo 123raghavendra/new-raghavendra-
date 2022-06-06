2863142b4a4737d9a815eeacb8d01e3ca0ac0339
unknown <raghavendra123xworkz@gmail.com>
2022-06-06 17:58:15 +0530

    class and object

---
+++ class and object/student.java
@@ -0,0 +1,9 @@
+class student{
+String name;
+String id;
+String dob;
+int age;
+void printdetails(){
+System.out.println("name:"+name+"\n"+"id:"+id+"\n"+"dob:"+dob+"\n"+"age:"+age+"\n");
+}
+}
---
+++ class and object/studentdemo.java
@@ -0,0 +1,17 @@
+class studentdemo{
+public static void main (String[] args){
+student ramya = new student();
+ramya.name="ramya";
+ramya.id="a01";
+ramya.dob="12/11/11";
+ramya.age=22;
+ramya.printdetails();
+
+student raju=new student();
+raju.name="raju";
+raju.id="b001";
+raju.dob="22/1/1999";
+raju.age=30;
+raju.printdetails();
+}
+}
