67dba47ba9f33103a7753189dc964f7fa2e4e705
User <raghavendra123xworkz@gmail.com>
2022-06-07 09:19:16 +0530

    has a relationship(COMPOSITION)

---
+++ java/src/oops/hasarelationship(COMPOSITION)/heart.java
@@ -0,0 +1,20 @@
+package oops;
+
+public class heart {
+	
+		String colour;
+		String btimes;
+		
+		void pumpblood() {
+			System.out.println("Heart pumps blood to all organs");
+		}
+		
+		void receiveblood (){
+			System.out.println("Heart recives blood from blue veins..!");
+			
+			
+		}
+
+	}
+
+
---
+++ java/src/oops/hasarelationship(COMPOSITION)/human.java
@@ -0,0 +1,21 @@
+package oops;
+
+ public class human {
+
+	 String haircolour;
+		String eyecolour;
+		String location;
+		String lang;
+		 heart heart;
+		
+		void see() {
+			System.out.println("Human eye is better than any camera");
+		}
+		void dark() {
+			System.out.println("Human eye can't see in the dark...!");		
+				
+			
+		}
+
+	}
+
