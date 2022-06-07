603ad54f26e131f965ffdcaad614a157ede94e50
User <raghavendra123xworkz@gmail.com>
2022-06-07 11:48:00 +0530

    example for constructer

---
+++ java/src/oops/Constructors/Paramaterizedconstructer/Gun.java
@@ -0,0 +1,19 @@
+package oops.Constructors.Paramaterizedconstructer;
+
+public class Gun {
+	int noofbullet;
+	double weight;
+	String colour;
+	
+	Gun(int bullet,Double wt,String clr){
+	noofbullet=bullet;
+	weight=wt;
+	colour=clr;
+	
+	
+	}
+	//void show() {
+		//System.out.println(noofbullet+ " "+weight +" "+colour);
+	}
+
+
---
+++ java/src/oops/Constructors/defaultconstructor/gun.java
@@ -0,0 +1,18 @@
+package oops.Constructors.defaultconstructor;
+
+public class gun {
+           gun(){
+			System.out.println("Default constructer");
+		}	
+		//class Default constructer
+			public static void main(String[] args) {
+				gun gun =new gun();
+				
+				
+				
+			}
+		
+		
+		
+		}
+
