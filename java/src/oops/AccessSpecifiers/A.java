0ca86417865a5d96f0e62d59bd1e2822df742910
User <raghavendra123xworkz@gmail.com>
2022-06-14 23:21:40 +0530

    access specifiers on public

---
+++ java/src/oops/AccessSpecifiers/A.java
@@ -0,0 +1,8 @@
+package oops.AccessSpecifiers;
+
+public class A {
+	public void msg() {
+	System.out.println("Hello");}  
+	}  
+
+
---
+++ java/src/oops/AccessSpecifiers/B.java
@@ -0,0 +1,11 @@
+package oops.AccessSpecifiers;
+
+	import oops.AccessSpecifiers.*;  
+	  
+	class B extends A{  
+	  public static void main(String args[]){  
+	   A obj = new A();  
+	   obj.msg();  
+	  }  
+	}  
+
