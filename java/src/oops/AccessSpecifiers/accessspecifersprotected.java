25b98477f6b69f446225df67bb3d0461ded9b1d9
User <raghavendra123xworkz@gmail.com>
2022-06-14 23:33:49 +0530

    access specifiers program on  protected keyword

---
+++ java/src/oops/AccessSpecifiers/accessspecifersprotected.java
@@ -0,0 +1,8 @@
+package oops.AccessSpecifiers;
+
+public class accessspecifersprotected {  
+		protected void msg(){
+		System.out.println("Hello,I am Raghavendra");}  
+		}  
+
+
---
+++ java/src/oops/AccessSpecifiers/accessspecifiersprotecteddemo.java
@@ -0,0 +1,12 @@
+package oops.AccessSpecifiers;
+
+	import oops.AccessSpecifiers.*;  
+public class accessspecifiersprotecteddemo extends accessspecifersprotected {
+	  
+	  public static void main(String args[]){  
+ accessspecifiersprotecteddemo  obj = new accessspecifiersprotecteddemo();  
+	  obj.msg();  
+	  }  
+	}  
+
+
