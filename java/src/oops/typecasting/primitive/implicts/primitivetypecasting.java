12eb856032371d8cb7e4faa6edb8784ff9341f40
User <raghavendra123xworkz@gmail.com>
2022-06-15 16:32:35 +0530

    example of primitive type casting

--- java/src/oops/typecasting/primitive/implicts/primitivetypecasting.java
+++ java/src/oops/typecasting/primitive/implicts/primitivetypecasting.java
@@ -4,12 +4,12 @@ public class primitivetypecasting {
 	public static void main (String [] args) {
 		//implicts/widening/automatic
 		
-		byte b=10;
+		byte b=10;                         //HINT -byte->short->int->long->float->double.
 		short s=b;
 		System.out.println("b:"+b);
 		System.out.println("s:"+s);    
     
-		// explicit/narrowing/manual
+		// explicit/narrowing/manual     //HINT- double->float->long->int->short->byte
         short s1=100;
        byte b1=(byte)s1;
       System.out.println("s1:"+s1);
