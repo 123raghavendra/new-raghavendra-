382eefb89bf48ed0b8286ec1cd55e98ea9523b92
User <raghavendra123xworkz@gmail.com>
2022-06-16 10:00:42 +0530

    example for converation of  int to char

---
+++ java/src/oops/typecasting/primitive/implicts/inttocharAssignment.java
@@ -0,0 +1,14 @@
+package oops.typecasting.primitive.implicts;
+
+public class inttocharAssignment {
+	public static void main(String [] args) {
+		int a=110;
+		char r=(char)a;
+		System.out.println("r:"+r);
+	
+		
+int b=90;
+char s=(char)b;
+System.out.println("s:"+s);
+}
+}
