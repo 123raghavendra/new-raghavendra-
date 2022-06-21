3d59db1b174675fa87100a3285906b595a9dc8a2
User <raghavendra123xworkz@gmail.com>
2022-06-21 23:27:49 +0530

    encapsulation example

---
+++ java/src/oops/encapsulation/dog.java
@@ -0,0 +1,56 @@
+package oops.encapsulation;
+
+public class dog {
+	
+	private String name;
+	private String colour;
+	private String bread;
+	private int age;
+	private double price;
+	
+public dog() {
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getColour() {
+		return colour;
+	}
+
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+
+	public String getBread() {
+		return bread;
+	}
+
+	public void setBread(String bread) {
+		this.bread = bread;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+
+}
---
+++ java/src/oops/encapsulation/dogtest.java
@@ -0,0 +1,20 @@
+package oops.encapsulation;
+
+public class dogtest {
+	public static void main(String[] args) {
+		dog dog =new dog();
+			dog.setName("kulfi");
+			dog.setColour("black");
+			dog.setBread("german");
+			dog.setAge(2);
+			dog.setPrice(20000.00);
+		
+		System.out.println("name"+dog.getName());
+		System.out.println("colour"+dog.getColour());
+		System.out.println("german"+dog.getBread());
+		System.out.println("age"+dog.getAge());
+		System.out.println("price"+dog.getPrice());
+		
+	} 
+
+}
