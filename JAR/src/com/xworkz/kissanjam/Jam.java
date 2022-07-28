e3435cb6c71a5f8e9ae2faaeba33d1a46296f4a5
User <raghavendra123xworkz@gmail.com>
2022-07-28 19:11:16 +0530

    Jdbc Creation of Jar and connection of jar

---
+++ JAR/src/com/xworkz/kissanjam/Jam.java
@@ -0,0 +1,11 @@
+package com.xworkz.kissanjam;
+
+public class Jam {
+	public static void main(String[] args) {
+		
+		Vendor v=new Vendor();
+		v.getName();
+		System.out.println(v.getName());
+	}
+
+}
---
+++ JAR/src/com/xworkz/kissanjam/Vendor.java
@@ -0,0 +1,12 @@
+package com.xworkz.kissanjam;
+
+public class Vendor {
+	Vendor(){
+		System.out.println("Default Constructer");
+		
+	}
+	
+	public String getName() {
+		return "google";
+	}
+}
