dc2a0ca4e0e145398ef9864a4a4083a38a72697f
User <raghavendra123xworkz@gmail.com>
2022-07-29 10:41:05 +0530

    Example For Jar expoert

---
+++ developer/src/com/xworkz/developer/Manoj.java
@@ -0,0 +1,10 @@
+package com.xworkz.developer;
+
+public class Manoj {
+	public static void main(String[] args) {
+		Raghu r =new Raghu();
+	r.getName();
+	System.out.println(r.getName());
+	}
+
+}
---
+++ developer/src/com/xworkz/developer/Raghu.java
@@ -0,0 +1,12 @@
+package com.xworkz.developer;
+
+public class Raghu {
+	Raghu(){
+		System.out.println("hi");
+	}
+
+	public String getName() {
+		return "ABHI";
+		
+	}
+}
