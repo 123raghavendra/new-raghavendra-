b8a40afac975e59691d947ad4a769360a44a5d69
User <raghavendra123xworkz@gmail.com>
2022-08-03 18:44:03 +0530

    By using Enum Insert the values

--- Bottle/src/com/xworkz/bottel/Grapes/GrapesInsert.java
+++ Bottle/src/com/xworkz/bottel/Grapes/GrapesInsert.java
@@ -6,19 +6,19 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class GrapesInsert {
-	
+
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-Class.forName("com.mysql.cj.jdbc.Driver");
-		
-		Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/Grapes","root","2ka17cv405@94");
+		Class.forName(DBpropertiesGrapes.NAME.getValue());
+
+		Connection connection = DriverManager.getConnection(DBpropertiesGrapes.URL.getValue(),
+				DBpropertiesGrapes.USERNAME.getValue(), DBpropertiesGrapes.PASSWORD.getValue());
 		System.out.println(connection);
-		
-		String insert="insert into grapesdemo values(10,100,'LightGreen','Round','Sweet')";
-		Statement statement=connection.createStatement();
-       int noOfRowsAffected=statement.executeUpdate(insert);
-       System.out.println(noOfRowsAffected);
-		 
-		
+
+		String insert = "insert into grapesdemo values(10,100,'LightGreen','Round','Sweet')";
+		Statement statement = connection.createStatement();
+		int noOfRowsAffected = statement.executeUpdate(insert);
+		System.out.println(noOfRowsAffected);
+
 	}
 
 }
